16b84f4613ebf93a0050e0f314a3770aeb58582d
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2018-02-05 14:28:27 -0600

    feature: add section to contain form wizard

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+  $(".rslides").responsiveSlides({
+    auto: true,             
+    speed: 300,            
+    timeout: 4000,          
+    pager: false,           
+    nav: true,           
+    pauseControls: true,    
+    prevText: "<",   
+    nextText: ">",       
+    manualControls: "",     
+    namespace: "rslides",    
+  });
+});
